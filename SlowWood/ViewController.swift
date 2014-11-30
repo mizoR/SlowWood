592fa985586aa2c759939d7147730a3e59f89498
mizokami <suzunatsu@yahoo.com>
2014-12-01 08:14:22 +0900

    Initial Commit

---
+++ SlowWood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlowWood
+//
+//  Created by ryutaro_mizokami on 2014/12/01.
+//  Copyright (c) 2014年 ryutaro_mizokami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
